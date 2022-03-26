25d32fb07e442d2bfec7343b7d4d74f96dd14ff0
jaechulhan <jaechulhan@gmail.com>
2022-03-25 20:58:37 -0400

    Name: jaechul.h Description: Logging Aspect

---
+++ src/main/java/net/prolancer/validation/common/logging/LoggingAspect.java
@@ -0,0 +1,35 @@
+package net.prolancer.validation.common.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("execution(* net.prolancer.validation.controller..*(..)))")
+    public Object logMethodExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        final StopWatch stopWatch = new StopWatch();
+
+        //Calculate method execution time
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution time
+        log.info("Execution time of "
+                + methodSignature.getDeclaringType().getSimpleName() // Class Name
+                + "." + methodSignature.getName() + " " // Method Name
+                + ":: " + stopWatch.getTotalTimeMillis() + " ms");
+
+        return result;
+    }
+}
